176423ab1b79ba53462ab4a0a74e3e6d5bc04740
velleda <16000407+notvelleda@users.noreply.github.com>
2023-10-08 23:08:20 -0500

    added more eeprom methods

--- src/component/eeprom.c
+++ src/component/eeprom.c
@@ -14,6 +14,10 @@ static int eeprom_get(lua_State *L, struct eeprom_data *data, int arguments_star
     return 1;
 }
 
+static int eeprom_set(lua_State *L, void *data, int arguments_start) {
+    return luaL_error(L, "eeprom contents are read-only");
+}
+
 static int eeprom_get_data(lua_State *L, struct eeprom_data *data, int arguments_start) {
     if (data->data == NULL)
         lua_pushnil(L);
@@ -38,20 +42,55 @@ static int eeprom_set_data(lua_State *L, struct eeprom_data *data, int arguments
     return 0;
 }
 
+static int eeprom_get_label(lua_State *L, struct eeprom_data *data, int arguments_start) {
+    if (data->label == NULL)
+        lua_pushnil(L);
+    else
+        lua_pushstring(L, data->label);
+    return 1;
+}
+
+static int eeprom_set_label(lua_State *L, struct eeprom_data *data, int arguments_start) {
+    const char *new_label = luaL_checkstring(L, arguments_start);
+
+    if (data->label != NULL)
+        free(data->label);
+
+    data->label = malloc(strlen(new_label) + 1);
+
+    if (data->label == NULL)
+        return luaL_error(L, "out of memory");
+
+    strcpy(data->label, new_label);
+
+    return 0;
+}
+
+static int eeprom_get_size(lua_State *L, struct eeprom_data *data, int arguments_start) {
+    lua_pushinteger(L, strlen(data->contents));
+    return 1;
+}
+
+static int eeprom_get_data_size(lua_State *L, void *data, int arguments_start) {
+    lua_pushinteger(L, 4096); /* arbitrary */
+    return 1;
+}
+
 struct eeprom_data *eeprom_init(void) {
     struct eeprom_data *data = malloc(sizeof(struct eeprom_data));
     assert(data != NULL);
 
     data->contents = NULL;
     data->data = NULL;
+    data->label = NULL;
     
     struct component *eeprom = new_component("eeprom", new_uuid(), data);
     add_method(eeprom, "get", eeprom_get);
-    // set
-    // getLabel
-    // setLabel
-    // getSize
-    // getDataSize
+    add_method(eeprom, "set", eeprom_set);
+    add_method(eeprom, "getLabel", eeprom_get_label);
+    add_method(eeprom, "setLabel", eeprom_set_label);
+    add_method(eeprom, "getSize", eeprom_get_size);
+    add_method(eeprom, "getDataSize", eeprom_get_data_size);
     add_method(eeprom, "getData", eeprom_get_data);
     add_method(eeprom, "setData", eeprom_set_data);
     // getChecksum
--- src/component/eeprom.h
+++ src/component/eeprom.h
@@ -3,6 +3,7 @@
 struct eeprom_data {
     const char *contents;
     const char *data;
+    const char *label;
 };
 
 struct eeprom_data *eeprom_init(void);
